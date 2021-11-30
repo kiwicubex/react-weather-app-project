4cface6e9fc7bfa222c6f7ac50a2a812553ae26d
kiwicubex <86155059+kiwicubex@users.noreply.github.com>
2021-12-01 00:35:00 +0800

    Added Github repo Url

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://brave-meninsky-cd508d.netlify.app/about.html">
+          Amanda Chan
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/kiwicubex/react-weather-app-project"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
