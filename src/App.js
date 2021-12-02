a2b9c531d487506a47ebd93fb0fd27c304587341
kiwicubex <86155059+kiwicubex@users.noreply.github.com>
2021-12-03 00:45:03 +0800

    Added the HTML of the weather app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://brave-meninsky-cd508d.netlify.app/about.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Amanda Chan{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/kiwicubex/react-weather-app-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
       </div>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://brave-meninsky-cd508d.netlify.app/about.html"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Amanda Chan{" "}
-        </a>
-        and is{" "}
-        <a
-          href="https://github.com/kiwicubex/react-weather-app-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
     </div>
   );
 }
