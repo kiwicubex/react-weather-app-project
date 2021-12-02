7dcb5f13a339876a0379418f8ee5cfa9212a3de1
kiwicubex <86155059+kiwicubex@users.noreply.github.com>
2021-12-03 00:25:20 +0800

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
+      </div>
       <footer>
         This project was coded by{" "}
         <a
