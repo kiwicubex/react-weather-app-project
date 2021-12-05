29047cf5f4df0851ef75cb24d29aeb540e48fb5a
kiwicubex <86155059+kiwicubex@users.noreply.github.com>
2021-12-05 14:31:49 +0800

    Added correct weather icon

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 export default function WeatherInfo(){
     return(
         <div className="WeatherInfo">
@@ -12,7 +13,8 @@ export default function WeatherInfo(){
         </ul>
         <div className="row mt-3">
           <div className="col-6">
-            <div className="clearfix">
+            <div className="clearfix"><div className="float-left">
+                <WeatherIcon code={props.data.icon}/></div>
               <img
                 src={weatherData.iconUrl}
                 alt={weatherData.description}
